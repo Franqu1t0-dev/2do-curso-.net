1e51c5b6a51bec5d52eeb26155f0c4eabbb07f0f
Franqu1t0-dev <francucho.candia@gmail.com>
2026-07-02 23:21:05 -0300

    product adapter and dto

---
+++ Fundamentals/02-Tipos-Basicos/DataTypes.cs
@@ -0,0 +1,18 @@
+class DataTypes
+{
+    //static void Main()
+    //{
+        // WriteLine("Hello, World!");
+        // int quantity=5;
+        // string message="Hello World";
+        // decimal price =19.99m;
+        // WriteLine($"Cantidad: {quantity}, Saludo: {message}, Precio: {price:C}");
+
+        // var gretting = "Hola";
+        // var percentage=20.00m;
+
+        // WriteLine($"Saludo: {gretting}, Porcentaje: {percentage}");
+
+    
+    //}
+}
--- Program.cs
+++ Program.cs
@@ -4,20 +4,14 @@ class Program
 {
     static void Main()
     {
-        WriteLine("Hello, World!");
-        int quantity=5;
-        string message="Hello World";
-        decimal price =19.99m;
-        WriteLine($"Cantidad: {quantity}, Saludo: {message}, Precio: {price:C}");
-
-        var gretting = "Hola";
-        var percentage=20.00m;
-
-        WriteLine($"Saludo: {gretting}, Porcentaje: {percentage}");
-
         var laptop=new Product("Laptop",1200);
         WriteLine(laptop.GetDescription());
         var soporte=new ServiceProduct("Soporte técnico",300,30);
         WriteLine(soporte.GetDescription());
+    
+        var product=new Product("Mouse Gamer",300);
+        var prodcutDto= ProductAdapter.ToDto(product);
+        WriteLine($"{prodcutDto.Name} - {prodcutDto.Price} - Código: {prodcutDto.Code}");
     }
 }
+    
